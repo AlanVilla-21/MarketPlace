8d8ae90c5c2fcd0a362fd5f83b7ead1e282b26dc
Camila <camila.avril.fuentes.flores@gmail.com>
2025-12-08 08:50:40 -0400

    loginxml-ultimo

--- app/src/main/java/com/example/marketplace/LoginUsuario.kt
+++ app/src/main/java/com/example/marketplace/LoginUsuario.kt
@@ -2,11 +2,24 @@ package com.example.marketplace
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.marketplace.databinding.ActivityLoginUsuarioBinding
+import com.example.marketplace.databinding.ActivityRegisterUsuarioBinding
 
 class LoginUsuario : AppCompatActivity() {
     private lateinit var binding : ActivityLoginUsuarioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_usuario)
+
+        binding = ActivityLoginUsuarioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignIn.setOnClickListener {
+            //
+        }
+
+        binding.btnGoogleLogin.setOnClickListener {
+            //
+        }
     }
 }
