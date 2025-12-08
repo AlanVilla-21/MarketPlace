f0843712a966908c2c08b5cdffef06e7d9d940f2
Camila <camila.avril.fuentes.flores@gmail.com>
2025-12-07 23:03:10 -0400

    registerxml

--- app/src/main/java/com/example/marketplace/RegisterUsuario.kt
+++ app/src/main/java/com/example/marketplace/RegisterUsuario.kt
@@ -5,16 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.marketplace.databinding.ActivityRegisterUsuarioBinding
 
 class RegisterUsuario : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterUsuarioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register_usuario)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityRegisterUsuarioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnContinuar.setOnClickListener {
+            //
+        }
+
+        binding.btnGoogle.setOnClickListener {
+            //
+        }
         }
     }
-}
