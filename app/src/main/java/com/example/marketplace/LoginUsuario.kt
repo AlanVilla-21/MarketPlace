68c4999512610107b658dc008fa215c11b72be01
Camila <camila.avril.fuentes.flores@gmail.com>
2025-12-07 20:24:46 -0400

    Login

---
+++ app/src/main/java/com/example/marketplace/LoginUsuario.kt
@@ -0,0 +1,11 @@
+package com.example.marketplace
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginUsuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_usuario)
+    }
+}
