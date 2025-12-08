ebd58ece6e7b115100833a3fcccb2ff6ace0be94
Alan V <villavicencioalan706@gmail.com>
2025-12-07 21:48:07 -0400

    strings and colors

--- app/src/main/java/com/example/marketplace/LoginUsuario.kt
+++ app/src/main/java/com/example/marketplace/LoginUsuario.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginUsuario : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginUsuarioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_usuario)
