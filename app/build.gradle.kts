6aa49c1eb071caa8ebd0911662300b61f019b335
Alan V <villavicencioalan706@gmail.com>
2025-12-09 09:15:08 -0400

    firebase login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-}
+        id("com.google.gms.google-services")
+    }
 
 android {
     namespace = "com.example.marketplace"
@@ -47,6 +48,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-auth")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/marketplace/RegisterUsuario.kt
+++ app/src/main/java/com/example/marketplace/RegisterUsuario.kt
@@ -1,27 +1,53 @@
 package com.example.marketplace
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.marketplace.databinding.ActivityRegisterUsuarioBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class RegisterUsuario : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterUsuarioBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityRegisterUsuarioBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
 
         binding.btnContinuar.setOnClickListener {
-            //
+            val correo = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            crearUsuario(correo, password)
         }
 
         binding.btnGoogle.setOnClickListener {
             //
         }
         }
+    fun crearUsuario (
+        correo:String,
+        password: String
+    ) {
+        auth.createUserWithEmailAndPassword(correo, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // Mi usuario se creo correctamente
+
+            } else {
+                //Nuestro usuario no se logio
+                Toast.makeText(
+                    baseContext,
+                    "No pudo logearse el usuario.",
+                    Toast.LENGTH_LONG,
+                ).show()
+            }
+        }
+    }
     }
