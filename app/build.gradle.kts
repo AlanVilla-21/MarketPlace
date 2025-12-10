f379687ce0fee8fce6ae5503257b2a882d3cc2d3
Sebas <sebas.2005.16vargas@gmail.com>
2025-12-10 09:15:58 -0400

    loginxml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-        id("com.google.gms.google-services")
-    }
+}
 
 android {
     namespace = "com.example.marketplace"
@@ -10,7 +9,7 @@ android {
         version = release(36)
     }
 
-     buildFeatures {
+    buildFeatures {
         viewBinding = true
     }
 
@@ -48,11 +47,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-    implementation("com.google.firebase:firebase-auth:23.1.0")
-    implementation(libs.firebase.auth.ktx)
-    implementation(platform(libs.firebase.bom))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //
 }
