1aa58f1afe52eae04beab94dc87a563ab30ed63c
Sebas <sebas.2005.16vargas@gmail.com>
2025-12-12 10:58:42 -0400

    menulateral

--- app/src/main/java/com/example/marketplace/MainActivity.kt
+++ app/src/main/java/com/example/marketplace/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.marketplace
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import com.example.marketplace.databinding.ActivityMainBinding
+import com.example.marketplace.databinding.LayoutMenuLateralBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var menuBinding: LayoutMenuLateralBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +19,42 @@ class MainActivity : AppCompatActivity() {
         binding.txtMenu.setOnClickListener {
             binding.drawerLayout.openDrawer(GravityCompat.START)
         }
-
-        // Si quieres luego clicks de abajo:
-        // binding.imgCasa.setOnClickListener { ... }
-        // binding.imgCarrito.setOnClickListener { ... }
-        // binding.imgPerfil.setOnClickListener { ... }
+        menuBinding.filaInicio.setOnClickListener {
+            startActivity(Intent(this, HomeUsuario::class.java))
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        menuBinding.filaVender.setOnClickListener {
+            startActivity(Intent(this, VenderProductoActivity::class.java))
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        menuBinding.filaCompras.setOnClickListener {
+            startActivity(Intent(this, HomeUsuario::class.java))
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        menuBinding.filaCategorias.setOnClickListener {
+            startActivity(Intent(this, categorias::class.java))
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        menuBinding.filaPerfil.setOnClickListener {
+            startActivity(Intent(this, PerfilUsuario::class.java))
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        menuBinding.filaCerrarSesion.setOnClickListener {
+            // volver al login y cerrar Main
+            startActivity(Intent(this, LoginUsuario::class.java))
+            finish()
+        }
+        binding.imgCasa.setOnClickListener {
+            startActivity(Intent(this, HomeUsuario::class.java))
+        }
+        binding.imgCarrito.setOnClickListener {
+            startActivity(Intent(this, HomeUsuario::class.java))
+        }
+        binding.imgPerfil.setOnClickListener {
+            startActivity(Intent(this, PerfilUsuario::class.java))
+        }
     }
+
 
     override fun onBackPressed() {
         if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
