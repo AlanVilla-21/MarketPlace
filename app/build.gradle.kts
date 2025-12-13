29061df200e3667600fabb97461e360e5588b179
Camila <camila.avril.fuentes.flores@gmail.com>
2025-12-13 09:50:20 -0400

    librerias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-        id("com.google.gms.google-services")
-    }
+    kotlin("plugin.serialization") version "1.9.0"
+    id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
+}
 
 android {
-    namespace = "com.example.marketplace"
-    compileSdk {
-        version = release(36)
-    }
-
-     buildFeatures {
-        viewBinding = true
-    }
+    namespace = "com.prograiii.myapplication"
+    compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.marketplace"
+        applicationId = "com.prograiii.myapplication"
         minSdk = 29
         targetSdk = 36
         versionCode = 1
@@ -40,18 +36,32 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
-    implementation("com.google.firebase:firebase-auth:23.1.0") // This works
-    implementation(libs.firebase.auth.ktx)
-    implementation(platform(libs.firebase.bom))
+
+    //Implementacion de Serializacion paso 2
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    implementation("androidx.room:room-runtime:2.8.3")
+    implementation("androidx.room:room-ktx:2.8.3")
+    ksp("androidx.room:room-compiler:2.8.3")
+
+
+    implementation("androidx.room:room-runtime:2.8.3")
+    implementation("androidx.room:room-ktx:2.8.3")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
